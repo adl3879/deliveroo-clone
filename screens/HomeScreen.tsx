24b1f0ec3a1ffb9112d0597a9394b49b2412cbc2
Toyosi Adekanmbi <oluwatoyosiadeleye4@gmail.com>
2023-01-02 22:44:16 +0100

    screens

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,44 @@
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+    ChevronDownIcon,
+    UserIcon,
+    AdjustmentsHorizontalIcon,
+    MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+
+export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+
+    return (
+        <SafeAreaView>
+            {/* Header */}
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{
+                        uri: "https://links.papareact.com/wru",
+                    }}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                />
+
+                <View>
+                    <Text className="font-bold text-gray-400 text-sm">
+                        Deliver Now
+                    </Text>
+                    <Text className="font-bold text-xl">
+                        Current Location
+                        {/* <ChevronDownIcon size={20} color="#00ccbb" /> */}
+                    </Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
